69d1b4a8b548c2bacee2af8d3f0841476e248cad
Chaimaazerouali <zeroualiiichaimaaa@gmail.com>
2024-04-14 16:28:08 -0700

    alx

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""Query Reddit API to determine subreddit sub count
 """
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
-def check_subreddit(subreddit):
-    """Check access to subreddit JSON data from the Reddit API and return 'OK'."""
-    url = f'https://www.reddit.com/r/{subreddit}.json'
-    headers = {'User-Agent': 'Mozilla/10.0/API'}
-    r = requests.get(url, headers=headers, allow_redirects=False)
 
-    if r.status_code == 200:
-        return "OK"
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/0.0.5"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
-        return "OK"  # Assuming that you need to return 'OK' regardless of the status
-
-print(check_subreddit("existing_subreddit"))
-print(check_subreddit("nonexisting_subreddit"))
-
-
+        return 0
